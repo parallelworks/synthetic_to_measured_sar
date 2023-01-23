edfabaee0cf5f16715bbd632496af2c1831f6e8c
avidalto <avidalto@hawk.iit.edu>
2023-01-23 15:26:48 -0600

    Integrate with new parsl_utils

--- parsl_wrapper.sh
+++ parsl_wrapper.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#!/bin/bash
 set -x
-# Can't get workflow type in PW to clone --recurse-submodules
+
+# Otherwise the submodule is fixed to a given commit...
 rm -rf parsl_utils
-git clone https://github.com/parallelworks/parsl_utils.git parsl_utils
-cp parsl_utils/kill.sh .
-cp parsl_utils/main.sh .
-bash main.sh $@
+git clone -b dev https://github.com/parallelworks/parsl_utils.git parsl_utils
+# Cant run a scripts inside parsl_utils directly
+bash parsl_utils/main.sh $@
