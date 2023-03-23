f250ac20e25e633dfa6518767eac274dfed8d5fa
avidalto <avidalto@hawk.iit.edu>
2023-03-23 13:49:43 -0500

    Confirming changes

--- workflow_apps.py
+++ workflow_apps.py
@@ -23,10 +23,7 @@ def prepare_rundir(run_dir, data_repo_dir="./SAMPLE_Public_Dist_A", inputs = [],
 @bash_app(executors=['compute_partition'])
 def mpi_add_noise(np, src_dir, dst_dir, noise_amount, inputs = [], stdout= 'std-noise.out', stderr = 'std-noise.err'):
     return '''
-        conda init bash 
-        conda create --name mpi4pi -y
         conda activate mpi4pi
-        conda install mpi4py pillow -y
         mpiexec -np {np} python models/mpi4py/mpi_add_noise.py {src_dir} {dst_dir} {noise_amount}
     '''.format(
             np = np,
