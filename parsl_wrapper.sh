698e232ae9e9a97e7baccf6bf16c86b0e438e2d6
Alvaro Vidal Torreira <avidalto@hawk.iit.edu>
2023-07-05 15:29:11 -0500

    Update parsl_wrapper.sh

--- parsl_wrapper.sh
+++ parsl_wrapper.sh
@@ -2,7 +2,7 @@
 
 # Confirm parsl_utils is cloned
 rm -rf parsl_utils
-git clone -b new-workflow https://github.com/parallelworks/parsl_utils.git parsl_utils
+git clone https://github.com/parallelworks/parsl_utils.git parsl_utils
 
 # Run the template
 ./parsl_utils/main.sh
